1837ff7b483f228f29f253157eb4c488565e5325
Kayla Sade <sade@Kaylas-MacBook-Pro.local>
2022-02-01 10:30:10 -0500

    built out home component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 
+// import components
+import Home from './components/Home';
+
 function App() {
   return (
     <div className="App">
-      <h1>secret family recipes</h1>
+      <Home/>
     </div>
   );
 }
