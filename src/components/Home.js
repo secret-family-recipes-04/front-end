4381c61ba0fc1b441b0217e82a014c86052c350d
Kayla Sade <92267062+kfam22@users.noreply.github.com>
2022-02-04 16:20:38 -0500

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -35,7 +35,7 @@ export default function Home() {
             </div>
             
             <div className='scroll'>
-                  <p>(scroll down)</p>
+                  <p>(scroll)</p>
             </div>
           </div>
 
